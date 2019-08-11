02e26a09d460fe2d1c1689e5eb123681a2d19c9a
Thomas van Latum <thomasvanlatum@192.168.2.13>
2019-08-11 17:11:03 +0200

    update

--- dataflow/main.py
+++ dataflow/main.py
@@ -19,7 +19,7 @@ class MyOptions(PipelineOptions):
 
 with beam.Pipeline(options=MyOptions()) as p:
     table_schema = 'PassengerId:INTEGER,Survived:INTEGER'
-    print(p.options.input)
+    print(p.options.get_all_options())
     lines = (p  | 'read' >> ReadFromText(p.options.input)
                 | 'Write Data to BigQuery' >> WriteToBigQuery(
                         p.options.output,
