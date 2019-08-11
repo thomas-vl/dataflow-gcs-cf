7ebd365c50c46f71860f8d3a0a755ba04f709338
Thomas van Latum <thomasvanlatum@thomass-mbp.home>
2019-08-11 18:45:24 +0200

    test argparser

--- dataflow/main.py
+++ dataflow/main.py
@@ -1,3 +1,5 @@
+import argparse
+import logging
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.value_provider import StaticValueProvider
@@ -6,24 +8,31 @@ from apache_beam.io import ReadFromText
 from apache_beam.io.gcp.bigquery import WriteToBigQuery
 from apache_beam.io.gcp.bigquery import BigQueryDisposition
 
-class MyOptions(PipelineOptions):
-    @classmethod
-    def _add_argparse_args(cls, parser):
-        parser.add_value_provider_argument(
-            '--input',
-            help='Path of the file to read from')
-        parser.add_argument(
-            '--output',
-            required=True,
-            help='Output file to write results to.')
+def run(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--output',
+        required=True,
+        help='Output file to write results to.')
+    parser.add_argument(
+        '--input',
+        help='Input file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+    pipeline_options = PipelineOptions(pipeline_args)
 
-with beam.Pipeline(options=MyOptions()) as p:
-    table_schema = 'PassengerId:INTEGER,Survived:INTEGER'
-    print(p.options.get_all_options())
-    lines = (p  | 'read' >> ReadFromText(p.options.input)
-                | 'Write Data to BigQuery' >> WriteToBigQuery(
-                        p.options.output,
-                        schema=table_schema,
-                        create_disposition=BigQueryDisposition.CREATE_IF_NEEDED,
-                        write_disposition=BigQueryDisposition.WRITE_TRUNCATE))
+    with beam.Pipeline(options=pipeline_options) as p:
+        table_schema = 'PassengerId:INTEGER,Survived:INTEGER'
+        print(known_args.input)
+        lines = (p  | 'read' >> ReadFromText(known_args.input)
+                    | 'Write Data to BigQuery' >> WriteToBigQuery(
+                            known_args.output,
+                            schema=table_schema,
+                            create_disposition=BigQueryDisposition.CREATE_IF_NEEDED,
+                            write_disposition=BigQueryDisposition.WRITE_TRUNCATE))
 
+        result = p.run()
+        result.wait_until_finish()
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
